58125c04444f32057e92cce934c8762db381272b
Fabio Curi <curif50@gmail.com>
2014-08-07 11:31:36 -0300

    Create ooCollection.h
    Add ooCollection class

---
+++ ooCollection.h
@@ -0,0 +1,43 @@
+/*
+ * ooCollection.h
+ *
+ *  Created on: Aug 7, 2014
+ *      Author: fcuri
+ */
+
+#ifndef OOCOLLECTION_H_
+#define OOCOLLECTION_H_
+
+#include "ooSimple.h"
+
+/**
+ * Generic collection storage
+ */
+#undef _ooClass
+#define _ooClass ooCollection
+ooClassD
+	ooImpAgregatorD(void);
+	void **arr;
+	int count;
+	int len;
+ooClassEndD()
+
+ooCtorD();
+ooDtorD();
+
+/**
+ * Iterator over ooCollection
+ */
+ooClass(oCollIterator)
+
+	ooImpIterator(oCollIterator, void);  //Next, HasNext
+
+	ooCollection *coll;
+	int idxCurrent;
+
+ooClassEnd(oCollIterator)
+
+ooCtor(oCollIterator, ooCollection *coll);
+ooDtor(oCollIterator);
+
+#endif /* OOCOLLECTION_H_ */
