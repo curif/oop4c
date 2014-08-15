2333dcf40c9d2134c9d6f49cd918e223e2c662c4
Fabio Curi <curif50@gmail.com>
2014-08-15 17:08:54 -0300

    Update ooString.h
    exceptions

--- ooString.h
+++ ooString.h
@@ -9,10 +9,17 @@
 #define OOSTRING_H_
 
 #include <string.h>
-
+#include <stdarg.h>
 #include "ooSimple.h"
 
 #define ooStringC(_st) (char*)(_st)->st
+
+//Use error handling
+ooExcepUse();
+
+//REdefine the stringable implementation to use the ooString class.
+#undef ooImpStringable
+#define ooImpStringable(_class) ooString *ooMethodDeclare(_class, ToString, ooString *st)
 
 ooClass(ooString)
 char *st;
@@ -24,8 +31,12 @@ ooPropertyGetDeclare(ooString, ooBoolean, IsEmpty);
 ooPropertyGetDeclare(ooString, int, Len);
 
 ooString *ooMethodDeclare(ooString, Cat, char *st);
+ooString *ooMethodDeclare(ooString, Catf, const char *format, ...);
+ooString *ooMethodDeclare(ooString, CatBuffer, char *buf, int len);
 ooString *ooMethodDeclare(ooString, Copy, char *st);
+ooString *ooMethodDeclare(ooString, Clean);
 ooBoolean ooMethodDeclare(ooString, Equal, char *st);
+
 
 ooClassEnd(ooString)
 
