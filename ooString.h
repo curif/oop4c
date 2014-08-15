2070c22630bc03a1c45bcdec02aabf1b11dfd3bd
Fabio Curi <curif50@gmail.com>
2014-08-15 17:20:59 -0300

    Update ooString.h
    Comparable.

--- ooString.h
+++ ooString.h
@@ -22,21 +22,21 @@ ooExcepUse();
 #define ooImpStringable(_class) ooString *ooMethodDeclare(_class, ToString, ooString *st)
 
 ooClass(ooString)
-char *st;
-int len;
+  char *st;
+  int len;
 
-ooImpTypeable(ooString);
+  ooImpTypeable(ooString);
+  ooImpComparable(ooString);
 
-ooPropertyGetDeclare(ooString, ooBoolean, IsEmpty);
-ooPropertyGetDeclare(ooString, int, Len);
+  ooPropertyGetDeclare(ooString, ooBoolean, IsEmpty);
+  ooPropertyGetDeclare(ooString, int, Len);
 
-ooString *ooMethodDeclare(ooString, Cat, char *st);
-ooString *ooMethodDeclare(ooString, Catf, const char *format, ...);
-ooString *ooMethodDeclare(ooString, CatBuffer, char *buf, int len);
-ooString *ooMethodDeclare(ooString, Copy, char *st);
-ooString *ooMethodDeclare(ooString, Clean);
-ooBoolean ooMethodDeclare(ooString, Equal, char *st);
-
+  ooString *ooMethodDeclare(ooString, Cat, char *st);
+  ooString *ooMethodDeclare(ooString, Catf, const char *format, ...);
+  ooString *ooMethodDeclare(ooString, CatBuffer, char *buf, int len);
+  ooString *ooMethodDeclare(ooString, Copy, char *st);
+  ooString *ooMethodDeclare(ooString, Clean);
+  ooBoolean ooMethodDeclare(ooString, Equal, char *st);
 
 ooClassEnd(ooString)
 
