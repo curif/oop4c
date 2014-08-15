b8e30b2b7d7df4f69763eba712ded8bccad61e17
Fabio Curi <curif50@gmail.com>
2014-08-15 17:23:12 -0300

    Update ooStringTest.c
    exceptions

--- test/ooStringTest.c
+++ test/ooStringTest.c
@@ -9,6 +9,28 @@
 #include "ooString.h"
 #include "ooCollection.h"
 
+ooExcepDefine();
+
+void printError(struct ooExcepError *e) {
+	printf("ERROR-------------\n");
+	printf("   Number: %i id:%s \n", e->no, e->id);
+	printf("   Program: %s\n", e->file);
+	printf("   Function: %s\n", e->function);
+	printf("   Line: %i\n", e->line);
+	printf("   Description: %s\n", e->desc);
+	return;
+}
+
+/**
+ * This function will be called when
+ * an unhandled exception raises.
+ */
+void unhandled(struct ooExcepError *e) {
+	printf("Unhandled exception\n");
+	printError(&ooLastError());
+	return;
+}
+
 int main() {
 	ooString *This;
 	ooString *is;
@@ -17,38 +39,51 @@ int main() {
 	ooString *fmt;
 	ooCollection *col;
 
-	This = ooNew(ooString, This, "This ");
-	is = ooNew(ooString, is, "is ");
-	aTest = ooNew(ooString, aTest, NULL);
-	aTest->Copy(aTest, "a ooString Test.");
+	ooExcepInit(&unhandled);
 
-	printf("%s%s%s\n", ooStringC(This), ooStringC(is), ooStringC(aTest));
-	printf("Len: %i, %i, %i\n", This->GetLen(This), is->GetLen(is), aTest->GetLen(aTest));
+	ooTry {
+		This = ooNew(ooString, This, "This ");
+		is = ooNew(ooString, is, "is ");
+		aTest = ooNew(ooString, aTest, NULL);
+		aTest->Copy(aTest, "a ooString Test.");
 
-	all = ooNew(ooString, all, NULL);
-	printf("All: %s\n", ooStringC(all->Cat(all, ooStringC(This))->Cat(all, ooStringC(is))->Cat(all, ooStringC(aTest))));
+		printf("%s%s%s\n", ooStringC(This), ooStringC(is), ooStringC(aTest));
+		printf("Len: %i, %i, %i\n", This->GetLen(This), is->GetLen(is), aTest->GetLen(aTest));
 
-	if (! all->Equal(all, "This is a ooString Test.")) {
-		printf("ERROR Equal\n");
+		all = ooNew(ooString, all, NULL);
+		printf("All: %s\n", ooStringC(all->Cat(all, ooStringC(This))->Cat(all, ooStringC(is))->Cat(all, ooStringC(aTest))));
+
+		if (! all->Equal(all, "This is a ooString Test.")) {
+			printf("ERROR Equal\n");
+		}
+
+		fmt = ooNew(ooString, fmt, NULL);
+		fmt->Catf(fmt, "%s %s %s", ooStringC(This), ooStringC(is), ooStringC(aTest));
+		printf("fmt result: %s\n", ooStringC(fmt));
+
 	}
-
-	fmt = ooNew(ooString, fmt, NULL);
-	fmt->Catf(fmt, "%s %s %s", ooStringC(This), ooStringC(is), ooStringC(aTest));
-	printf("fmt result: %s\n", ooStringC(fmt));
-
-	col = ooNew(ooCollection, col);
-
-	col->Add(col, This);
-	col->Add(col, is);
-	col->Add(col, aTest);
-
-	oCollIterator *i = col->GetIterator(col);
-	ooString *elem;
-	ooIterForEach(i,  col) {
-		elem = i->GetCurrent(i);
-		printf("Collection string member: %s\n", elem->st);
+	ooCatchAny() {
+		printError(&ooLastError());
 	}
-	ooDeleteFree(i); //remember to destroy iterator.
+	ooTryEnd;
+
+	ooTry {
+		col = ooNew(ooCollection, col);
+
+		col->Add(col, This)->Add(col, is)->Add(col, aTest);
+
+		oCollIterator *i = col->GetIterator(col);
+		ooString *elem;
+		ooIterForEach(i,  col) {
+			elem = i->GetCurrent(i);
+			printf("Collection string member: %s\n", elem->st);
+		}
+		ooDeleteFree(i); //remember to destroy iterator.
+	}
+	ooCatchAny() {
+		printError(&ooLastError());
+	}
+	ooTryEnd;
 
 	ooDeleteFree(This);
 	ooDeleteFree(is);
