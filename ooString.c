914b24541af5650af488efeec9f3342fe42a3f91
Fabio Curi <curif50@gmail.com>
2014-08-15 17:07:47 -0300

    Update ooString.c
    exceptions

--- ooString.c
+++ ooString.c
@@ -19,14 +19,53 @@ ooString *ooMethod(ooString, Cat, char *st) {
 		int l = strlen(st);
 		if (l) {
 			void *new = realloc(this->st, this->len + l + 1);
-			if (new) {
-				this->st = new;
-				strcpy(this->st + this->len, st);
-				this->len += l;
+			if (!new) {
+				ooRaise(Malloc);
 			}
+			this->st = new;
+			strcpy(this->st + this->len, st);
+			this->len += l;
 		}
 	}
-	return this;
+	return(this);
+}
+
+ooString *ooMethod(ooString, CatBuffer, char *buf, int l) {
+	if (buf && l) {
+		void *new = realloc(this->st, this->len + l + 1);
+		if (new) {
+			this->st = new;
+			memcpy(this->st + this->len, buf, l);
+			this->len += l;
+			this->st[this->len]=0;
+		}
+	}
+	return(this);
+}
+
+ooString *ooMethod(ooString, Clean) {
+	if (this->st) {
+		this->st[0]=0;
+		this->len=0;
+	}
+	return(this);
+}
+
+ooString *ooMethod(ooString, Catf, const char *format, ...) {
+	char *buf = malloc(1024);
+	if (buf) {
+	  va_list va;
+
+	  va_start(va, format);
+	  vsnprintf(buf, 1024, format, va);
+	  buf[1023]=0;
+	  va_end(va);
+
+	  this->Cat(this, buf);
+
+	  free(buf);
+	}
+	return(this);
 }
 
 ooBoolean ooMethod(ooString, Equal, char *st) {
@@ -55,7 +94,10 @@ ooCtor(ooString, char *st) {
 
 	ooMethodInit(ooString, Copy);
 	ooMethodInit(ooString, Cat);
+	ooMethodInit(ooString, Catf);
+	ooMethodInit(ooString, CatBuffer);
 	ooMethodInit(ooString, Equal);
+	ooMethodInit(ooString, Clean);
 	ooPropertyGetInit(ooString, IsEmpty);
 	ooPropertyGetInit(ooString, Len);
 	ooTypeable(ooString);
